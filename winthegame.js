576ee6645b5b4e6de0899f276235993487170ef4
crenslow <39916623+crenslow@users.noreply.github.com>
2019-11-11 11:53:33 -0800

    Create winthegame.js

---
+++ winthegame.js
@@ -0,0 +1,11 @@
+<!-- //place holder parameters until movement system known -->
+function winthegame(currX,currY, kolaX, kolaY ){
+	if(currX == kolaX && currY == kolaY)
+	{
+		alert("Congrats, you found the Koca-Kola recipe and won the game!");
+	}
+	else{
+		return;
+	}
+	
+}
