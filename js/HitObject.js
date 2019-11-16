abd7b582c9886712beb2941be157205705480490
angiemcgraw <56895781+angiemcgraw@users.noreply.github.com>
2019-11-15 22:50:01 -0800

    Update HitObject.js

--- js/HitObject.js
+++ js/HitObject.js
@@ -1,3 +1,1 @@
-/ * Used to see if OldSpice has encountered any objects
-  * Objects: celestial artifacts, mini-marts, stations, BadMax, etc.
-  * /
+<! -- // Used to see if OldSpice has encountered any objects; celestial artifacts, mini-marts, stations, BadMax, etc. -->
