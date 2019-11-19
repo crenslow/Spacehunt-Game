56917c2f55ba839e77fa6cc336d2612be19eda32
liang909 <44662100+liang909@users.noreply.github.com>
2019-11-18 23:08:00 -0800

    Update Sensors.js

--- Sensors.js
+++ Sensors.js
@@ -6,29 +6,68 @@
 function Sensors(currX,currY,supplies, sensors_tpye, Celestial_Map)
 {
 	alert("you deploys sensors for current cp");
-	supplies = supplies*0.8; <!-- consumed 2% ?  -->
-
+	supplies = supplies-2?
 	if(sensors_type == 1)  <!-- basic -->
 	{
-		for X in range(currX-2,currX+2):
-			for Y in range(currY-2,currY+2):
-				if (X == celestialX && Y == celestialY):
+		var i;
+		var j;
+		for(i=currX-2;i<currX+2;i++){
+			for(j=currY-2;j<currY+2;j++){
+				if(i==celestialX && j==celestialY){
 					//check if (celestialX,celestialY) in Celestial_map
 					//add all new (celestialX,celestialY) in Celestial Map
+				}
+				else
+					break;
+			}
+		}
 
 	}
 
 	else if(sensors_tpye == 2) <!-- enhanced -->
 	{
-		for X in range(currX-5,currX+5):
-			for Y in range(currY-5,currY+5):
-				if (X == celestialX && Y == celestialY):
+		var i;
+		var j;
+		for(i=currX-5;i<currX+5;i++){
+			for(j=currY-5;j<currY+5;j++){
+				if(i==currX-5 && j==currY+4)
+					continue;
+				if(i==currX-5 &&j==currY+5)
+					continue;
+				if(i==currX-5 &&j==currY-4)
+					continue;
+				if(i==currX-5 &&j==currY-5)
+					continue;
+				if(i==currX+5 && j==currY+4)
+					continue;
+				if(i==currX+5 &&j==currY+5)
+					continue;
+				if(i==currX+5 &&j==currY-4)
+					continue;
+				if(i==currX+5 &&j==currY-5)
+					continue;
+				if(i==currX-4 &&j==currY+5)
+					continue;
+				if(i==currX-4 &&j==currY-5)
+					continue;
+				if(i==currX+4 &&j==currY+5)
+					continue;
+				if(i==currX+4 &&j==currY-5)
+					continue;
+
+				if(i==celestialX && j==celestialY){
+
 					//check if (celestialX,celestialY) in Celestial_map
-					//add all new (celestialX,celestialY) in Celestial Map		
+					//add all new (celestialX,celestialY) in Celestial Map
+				}
+				else 
+					break;
+			}
+		}		
 	}
 }
 
 //how to get celestialX,celestialY form map
 //how to check in celestial map
-//consider different direction(if direction is 30,45,60 degree have one node shouldn't be search)
+//consider different direction
 //every direction search 5 ,like a circle
