9a915ffa89f32d1581a007897644eed79eeffcd1
angiemcgraw <56895781+angiemcgraw@users.noreply.github.com>
2019-11-19 19:02:59 -0800

    Update Mode.js

--- js/Mode.js
+++ js/Mode.js
@@ -5,17 +5,22 @@ function GameMode (params) {
     gameObj.tickObjects = [];
     gameObj.isGameover = false;
     
+    // win the game; the message that displays when player wins
+    // the game
     gameObj.GameWinner = function(reason) {
         alert(reason + "\nWinner!" );
         location.reload();
     }
-
+    
+    // lose the game; the message that displays when player loses 
+    // the game
     gameObj.GameOver = function(reason) {
         this.isGameover = true;
         alert(reason + "\nGame over!");
         location.reload();
     }
    
+    // could be discarded; was used for testing
     gameObj.tick = function () {
         console.log("Using tick objects.")
         let i;
