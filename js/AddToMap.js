750e7db8277030d898949db888ce8c608477082a
angiemcgraw <56895781+angiemcgraw@users.noreply.github.com>
2019-11-27 22:00:41 -0800

    Update AddToMap.js

--- js/AddToMap.js
+++ js/AddToMap.js
@@ -1,134 +1,152 @@
-/* Add items to the map */
 function PopulateMap(gameMap) {
+    generateEniac(gameMap);
+    generateRyzen(gameMap);
     generateCeleron(gameMap);
     generateXeon(gameMap);
-    generateRyzen(gameMap);
-    generateEniac(gameMap);
-    generateBadMax(gameMap);
-    generateRecipe(gameMap);
 
-    // load objects; asteroid, stations, freighters, other celestial objects
-    
-    // * ASTEROID *
+    // load celestial objects
     if (window.gameData.asteroidRandom) {
-        for(let i = 0; i < 10; ++i) {
-            let objCoordx = Math.ceil(Math.random() * (gameMap.size));
-            let objCoordy = Math.ceil(Math.random() * (gameMap.size));
-            generateCelestialObjects(gameMap, 0, objCoordx, objCoordy);
+        for (let i = 0; i < 100; ++i) {
+            let x = Math.ceil(Math.random() * (gameMap.size));
+            let y = Math.ceil(Math.random() * (gameMap.size));
+            generateCelestialObjects(gameMap, 4, x, y);
         }
     } else {
-        for(let coords of window.gameData.asteroids) {
-            let objCoordx = coords[0];
-            let objCoordy = coords[1];
-            generateCelestialObjects(gameMap, 0, objCoordx, objCoordy);
-        }
-    }
-
-    // * METEOR SHOWERS *
-    if(window.gameData.meteorRandom) {
-        for(let i = 0; i < 10; ++i) {
-            let objCoordx = Math.ceil(Math.random() * (gameMap.size));
-            let objCoordy = Math.ceil(Math.random() * (gameMap.size));
-            generateCelestialObjects(gameMap, 2, objCoordx, objCoordy); 
-        }
-    } else {
-        for(let coords of window.gameData.meteors) {
-            let objCoordx = coords[0];
-            let objCoordy = coords[1];
-            generateCelestialObjects(gameMap, 2, objCoordx, objCoordy);
+        for (let coords of window.gameData.asteroids) {
+            let x = coords[0];
+            let y = coords[1];
+            generateCelestialObjects(gameMap, 4, x, y);
         }
     }
     
-    // * FREIGHTERS *
     if (window.gameData.freighterRandom) {
-        for(let i = 0; i < 10; ++i) {
-            let objCoordx = Math.ceil(Math.random() * (gameMap.size));
-            let objCoordy = Math.ceil(Math.random() * (gameMap.size));
-            generateCelestialObjects(gameMap, 1, objCoordx, objCoordy);
+        for (let i = 0; i < 100; ++i) {
+            let x = Math.ceil(Math.random() * (gameMap.size));
+            let y = Math.ceil(Math.random() * (gameMap.size));
+            generateCelestialObjects(gameMap, 5, x, y);
         }
     } else {
-        for(let coords of window.gameData.freighters) {
-            let objCoordx = coords[0];
-            let objCoordy = coords[1];
-            generateCelestialObjects(gameMap, 1, objCoordx, objCoordy);
+        for (let coords of window.gameData.freighters) {
+            let x = coords[0];
+            let y = coords[1];
+            generateCelestialObjects(gameMap, 5, x, y);
         }
     }
 
-    // * STATIONS *
+    if (window.gameData.meteorRandom) {
+        for (let i = 0; i < 100; ++i) {
+            let x = Math.ceil(Math.random() * (gameMap.size));
+            let y = Math.ceil(Math.random() * (gameMap.size));
+            generateCelestialObjects(gameMap, 6, x, y); 
+        }
+    } else {
+        for (let coords of window.gameData.meteors) {
+            let x = coords[0];
+            let y = coords[1];
+            generateCelestialObjects(gameMap, 6, x, y);
+        }
+    }
+
     if (window.gameData.stationRandom) {
-        for(let i = 0; i < 10; ++i) {
-            let objCoordx = Math.ceil(Math.random() * (gameMap.size));
-            let objCoordy = Math.ceil(Math.random() * (gameMap.size));
-            generateCelestialObjects(gameMap, (i % 4), objCoordx, objCoordy);
+        for (let i = 0; i < 100; ++i) {
+            let x = Math.ceil(Math.random() * (gameMap.size));
+            let y = Math.ceil(Math.random() * (gameMap.size));
+            generateCelestialObjects(gameMap, (i % 4), x, y);
         }
     } else {
-        for(let coords of window.gameData.stations) {
-            let objCoordx = coords[0];
-            let objCoordy = coords[1];
+        for (let coords of window.gameData.stations) {
+            let x = coords[0];
+            let y = coords[1];
             let stationType = Math.floor(Math.random() * 4);
-            generateCelestialObjects(gameMap, stationType, objCoordx, objCoordy);
+            generateCelestialObjects(gameMap, stationType, x, y);
         }
     }
-    
 }
 
-/* populates a saved map with the state it was left in */
-function PopulateSavedMap ( gameMap, savedMap ) {
+function PopulateSavedMap (gameMap, savedMap) {
 
 }
 
-/* generates celestial objects */
 function generateCelestialObjects (gameMap, type, celestX, celestY) {
-   switch(type) {
-       case 0:
-            mapObj = new Asteroid();
-            break;
-       case 1: 
-            mapObj = new AbFreighter();
-            break;
-       case 2: 
-            mapObj = new MeteorShower();
-            break;
-       case 3: 
+    switch ( type ) {
+        case 0:
             mapObj = new SpaceStation([new MuskTesla(100, 1000), new RepairDepot, new MiniMart()]);
             break;
-       case 4: 
+
+        case 1:
             mapObj = new SpaceStation([new MuskTesla(100, 1000), new RepairDepot()]);
             break;
-       case 5: 
-            mapObj = new SpaceStation([new MuskTesla( 100, 1000 ), new MiniMart()]);
+
+        case 2:
+            mapObj = new SpaceStation([new MuskTesla(100, 1000), new MiniMart()]);
             break;
-       case 6: 
-            mapObj = new SpaceStation([new MuskTesla( 100, 1000 )]);
+
+        case 3:
+            mapObj = new SpaceStation([new MuskTesla(100, 1000)]);
             break;
-   }
-   updateLogs(gameMap, mapObj, celestX, celestY);
+
+        case 4:
+            mapObj = new Asteroid();
+            break;
+
+        case 5:
+            mapObj = new AbFreighter();
+            break;
+
+        case 6:
+            mapObj = new MeteorShower();
+            break;
+    }
+    updateLogs(gameMap, mapObj, celestX, celestY);
 }
 
-/* save the current state of the game */
-function updateLogs ( gameMap, mapObj, objCoordX, objCoordY ) {
+
+function updateLogs (gameMap, mapObj, objCoordX, objCoordY) {
     console.log( 'Placed ' + mapObj.objType + " at position: " + objCoordX + ' ' + objCoordY );
     gameMap.add( mapObj, objCoordX, objCoordY );
     saveMap( gameData, mapObj, objCoordX, objCoordY );
 }
 
-// * CELERON *
-// generate celeron at a random x and y position, call the
-// generateCeleronAtLocation function to actually place Celeron 
-// on the map
-function generateCeleron( gameMap ) {
-    let randomCellX = Math.ceil(Math.random() * (gameMap.size));
-    let randomCellY = Math.ceil(Math.random() * (gameMap.size));
-    generateCeleronAtLocation(gameMap, randomCellX, randomCellY);
+function generateEniac (gameMap) {
+    generateEniacAtLocation(gameMap, 0, 0);
 }
 
-// places Celeron on the map
+function generateEniacAtLocation(gameMap, eniacCoordX, eniacCoordY) {
+    mapObj = new Eniac();
+    mapObj.x = eniacCoordX;
+    mapObj.y = eniacCoordY;
+    updateLogs( gameMap, mapObj, eniacCoordX, eniacCoordY );
+}
+
+function generateRyzen (gameMap) {
+    let randomRyzenX = Math.ceil(Math.random() * (gameMap.size));
+    let randomRyzenY = Math.ceil(Math.random() * (gameMap.size));
+    generateRyzenAtLocation(gameMap, randomRyzenX, randomRyzenY)
+}
+
+function generateRyzenAtLocation(gameMap, ryzenCoordX, ryzenCoordY) {
+    mapObj = new Ryzen();
+    if ( gameData.ryzenX || gameData.ryzenX === 0 )
+        ryzenCoordX = gameData.ryzenX;
+    if ( gameData.ryzenY || gameData.ryzenY === 0 )
+        ryzenCoordY = gameData.ryzenY;
+    mapObj.x = ryzenCoordX;
+    mapObj.y = ryzenCoordY;
+    updateLogs(gameMap, mapObj, ryzenCoordX, ryzenCoordY );
+    gazePopulate( mapObj, ryzenCoordX, ryzenCoordY );
+}
+
+function generateCeleron(gameMap) {
+    let randomCelX = Math.ceil(Math.random() * (gameMap.size));
+    let randomCelY = Math.ceil(Math.random() * (gameMap.size));
+    generateCeleronAtLocation(gameMap, randomCelX, randomCelY);
+}
+
 function generateCeleronAtLocation(gameMap, celeronCoordX, celeronCoordY) {
-    // if the location has nothing, add Celeron to the map
     mapObj = new Celeron();
-    if (gameData.celeronX || gameData.celeronX === 0)
+    if ( gameData.celeronX || gameData.celeronX === 0 )
         celeronCoordX = gameData.celeronX;
-    if (gameData.celeronY || gameData.celeronY === 0)
+    if ( gameData.celeronY || gameData.celeronY === 0 )
         celeronCoordY = gameData.celeronY;
     mapObj.x = celeronCoordX;
     mapObj.y = celeronCoordY;
@@ -136,33 +154,12 @@ function generateCeleronAtLocation(gameMap, celeronCoordX, celeronCoordY) {
     gazePopulate(mapObj, celeronCoordX, celeronCoordY);
 }
 
-// * ENIAC *
-// generate eniac at a random x and y position, call the
-// generateEniacAtLocation function to actually place eniac 
-// on the map
-function generateEniac (gameMap) {
-    generateEniacAtLocation(gameMap, 0, 0);
-}
-
-// places Eniac on the map
-function generateEniacAtLocation(gameMap, eniacCoordX, eniacCoordY) {
-    mapObj = new Eniac();
-    mapObj.x = eniacCoordX;
-    mapObj.y = eniacCoordY;
-    updateLogs( gameMap, mapObj, eniacCoordX, eniacCoordY);
-}
-
-// * XEON *
-// generate xeon at a random x and y position, call the
-// generateXeonAtLocation function to actually place Xeon 
-// on the map
-function generateXeon(gameMap) {
+function generateXeon (gameMap) {
     let randomXeonX = Math.ceil(Math.random() * (gameMap.size));
     let randomXeonY = Math.ceil(Math.random() * (gameMap.size));
     generateXeonAtLocation(gameMap, randomXeonX, randomXeonY);
 }
 
-// places Xeon on the map
 function generateXeonAtLocation(gameMap, xeonCoordX, xeonCoordY) {
     mapObj = new Xeon();
     if (gameData.xeonX || gameData.xeonX === 0)
@@ -173,49 +170,4 @@ function generateXeonAtLocation(gameMap, xeonCoordX, xeonCoordY) {
     mapObj.y = xeonCoordY;
     updateLogs(gameMap, mapObj, xeonCoordX, xeonCoordY);
     gazePopulate(mapObj, xeonCoordX, xeonCoordY);
-}
-
-// * RYZEN *
-// generate ryzen at a random x and y position, call the
-// generateRyzenAtLocation function to actually place Ryzen 
-// on the map
-function generateRyzen ( gameMap ) {
-    let randRyzenX = Math.ceil(Math.random() * (gameMap.size));
-    let randRyzenY = Math.ceil(Math.random() * (gameMap.size));
-    generateRyzenAtLocation(gameMap, randRyzenX, randRyzenY)
-}
-
-// places Ryzen on the map
-function generateRyzenAtLocation(gameMap, ryzenCoordX, ryzenCoordY) {
-    mapObj = new Ryzen();
-    if(gameData.ryzenX || gameData.ryzenX === 0)
-        ryzenCoordX = gameData.ryzenX;
-    if(gameData.ryzenY || gameData.ryzenY === 0)
-        ryzenCoordY = gameData.ryzenY;
-    mapObj.x = ryzenCoordX;
-    mapObj.y = ryzenCoordY;
-    updateLogs(gameMap, mapObj, ryzenCoordX, ryzenCoordY);
-    gazePopulate(mapObj, ryzenCoordX, ryzenCoordY);
-}
-
-// RECIPE; generate recipe at a random location
-// not implemented yet, but essentially follows the same idea as generating the planets
-function generateRecipe ( gameMap ) {
-   
-}
-
-// not implemented yet, but essentially follows the same idea as generating the planets
-function generateRecipeAtLoation(gameMap, recipeCoordX, recipeCoordY){
-
-}
-
-// BAD MAX; generate Bad Max at a random location 
-// not implemented yet, but essentially follows the same idea as generating the planets
-function generateBadMax ( gameMap ) {
-   
-}
-
-// not implemented yet, but essentially follows the same idea as generating the planets
-function generateBadMaxAtLocation(gameMap, badMaxCoordX, badMaxCoordY){
-
 }
