6d8a0e8fcccbf321d69493342e97d68192e78ad3
angiemcgraw <56895781+angiemcgraw@users.noreply.github.com>
2019-12-02 21:05:31 -0800

    space station 1 implementation

---
+++ js/Station1.js
@@ -0,0 +1,22 @@
+/* SPACE STATION 1 */
+
+// each station has a different chance for winning credits
+
+function Station1(credit)
+{ 
+    if(gameMap.contents(oldSpice.x, oldSpice.y) != undefined){
+        if(gameMap.contents(oldSpice.x, oldSpice.y).name == "SpaceStation1"){
+            var playGameOfChance = Math.floor(Math.random() * Math.floor(3)); 
+            if(playGameOfChance < 2) { 
+                //if(confirm("You have been asked to play a game of chance. Would you like to play?")) { 
+                   var winnings = Math.floor(Math.random() * Math.floor(200));
+                   credit = credit + winnings; 
+                   alert("You've been asked to play a game of chance!"); 
+                   alert("You have won " + winnings + " credit(s)!"); 
+                   return [credit];
+                //}
+            }
+        }
+    }
+    return [credit];
+}
